04e2a18ecc2ff65e27189a1917b13e9b61c61205
Former <oncwnuEFk3lVc8alAlO6VwTiNN9I@git.weixin.qq.com>
2019-08-05 21:49:27 +0800

    1.3
    抽奖模块增加公司简介

---
+++ v_prize/v_prize/inc/web/system_setting.inc.php
@@ -0,0 +1,32 @@
+<?php
+
+global $_W,$_GPC;
+
+if($_W['ispost']==true){
+    if($_GPC['id']==null){
+        pdo_insert($this->system_setting,
+            [
+                "title"=>$_GPC['title'],
+                "slideshow"=>iserializer($_GPC['slideshow']),
+                "appid"=>$_GPC['appid'],
+                "appsecret"=>$_GPC['appsecret']
+            ]
+        );
+    }else{
+        pdo_update($this->system_setting,
+            [
+                "title"=>$_GPC['title'],
+                "slideshow"=>iserializer($_GPC['slideshow']),
+                "appid"=>$_GPC['appid'],
+                "appsecret"=>$_GPC['appsecret']
+            ],["id"=>$_GPC['id']]
+        );
+    }
+
+    message("保存成功",$this->createWebUrl('system_setting'),"success");
+}
+
+$system_setting = pdo_getall($this->system_setting)[0];
+$system_setting['slideshow'] = iunserializer($system_setting['slideshow']);
+
+include_once $this->template('system_setting');
