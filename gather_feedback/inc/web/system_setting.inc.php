77f44bf99242640f9b4d62a329e1b9944030d32f
Former <oncwnuEFk3lVc8alAlO6VwTiNN9I@git.weixin.qq.com>
2019-11-27 10:37:29 +0800

    1.6.0
    信息收集正式版本

--- gather_feedback/inc/web/system_setting.inc.php
+++ gather_feedback/inc/web/system_setting.inc.php
@@ -3,18 +3,19 @@
 global $_W, $_GPC;
 
 $arr = [
-    'title'=>$_GPC['title'],
-    'image'=>$_GPC['image']
+    'title' => $_GPC['title'],
+    'image' => $_GPC['image'],
+    'last_submit_text' => $_GPC['last_submit_text']
 ];
 
 /*提交*/
-if($_W['ispost']){
+if ($_W['ispost']) {
     if ($_GPC['id'] === '') {
-        pdo_insert('ims_gather_feedback_system_setting',$arr);
-    }else{
-        pdo_update('ims_gather_feedback_system_setting',$arr,['id'=>$_GPC['id']]);
+        pdo_insert('ims_gather_feedback_system_setting', $arr);
+    } else {
+        pdo_update('ims_gather_feedback_system_setting', $arr, ['id' => $_GPC['id']]);
     }
-    message('保存成功',$this->createWebUrl('system_setting'),'success');
+    message('保存成功', $this->createWebUrl('system_setting'), 'success');
 }
 
 $system_setting = pdo_getall('ims_gather_feedback_system_setting')[0];
