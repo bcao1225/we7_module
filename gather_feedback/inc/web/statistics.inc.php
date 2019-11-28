891fdd3f112705e16b5e50e655b4e260615848bd
Former <oncwnuEFk3lVc8alAlO6VwTiNN9I@git.weixin.qq.com>
2019-11-28 11:20:57 +0800

    1.6.4
    收集反馈修复一个小问题，问题导致安装模块sql语句报错

--- gather_feedback/inc/web/statistics.inc.php
+++ gather_feedback/inc/web/statistics.inc.php
@@ -2,6 +2,23 @@
 
 global $_GPC,$_W;
 
+/*所有用户提交填空题的集合*/
+$text_list = [];
+
+/*获取提交人的信息*/
+$user_list = pdo_fetchall('SELECT * FROM ims_gather_feedback_user WHERE submit_id IS NOT NULL');
+
+foreach ($user_list as $user_key => $user){
+    $submit = pdo_get('ims_gather_feedback_submit',['id'=>$user['submit_id']]);
+    $submit['data'] = iunserializer($submit['data']);
+
+    foreach ($submit['data'] as $submit_select){
+        /*每个选项中如果存在desc字段，表示这个是一个填空题提交的数据*/
+        if(array_key_exists('desc',$submit_select)){
+            array_push($text_list,$submit_select);
+        }
+    }
+}
 
 /*获取题目和对应的选项*/
 $parent_list = pdo_fetchall('SELECT * FROM ims_gather_feedback_question ORDER BY sort');
