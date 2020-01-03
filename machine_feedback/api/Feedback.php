7f01bc10a7d731ccc8fec1f91bd211de0b8bf18d
Former <oncwnuEFk3lVc8alAlO6VwTiNN9I@git.weixin.qq.com>
2020-01-03 14:11:22 +0800

    首页增加反馈信息

--- machine_feedback/api/Feedback.php
+++ machine_feedback/api/Feedback.php
@@ -17,10 +17,24 @@ class Feedback extends Api
             'machine_id' => $_GPC['machine_id'],
             'imgs' => $_GPC['imgs'],
             'video' => $_GPC['video'],
-            'text'=>$_GPC['text'],
+            'text' => $_GPC['text'],
             'create_time' => time()
         ]);
 
         $this->result(0, '提交成功', []);
+    }
+
+    public function feedback_by_id()
+    {
+        global $_GPC, $_W;
+
+        $feedback_list = pdo_fetchall('SELECT * FROM ims_machine_feedback_submit WHERE machine_id=' . $_GPC['id'] . ' ORDER BY create_time DESC');
+
+        foreach ($feedback_list as $key => $feedback) {
+            $feedback_list[$key]['user'] = pdo_get('ims_machine_feedback_user', ['id' => $feedback['user_id']]);
+            $feedback_list[$key]['create_time'] = date('Y-m-d', $feedback['create_time']);
+            $feedback_list[$key]['imgs'] = explode(',', $feedback['imgs']);
+        }
+        $this->result(0, '获取成功', $feedback_list);
     }
 }
