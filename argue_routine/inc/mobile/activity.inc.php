001f8e4e069b0f412ab1c73b935091ca87bb0b88
Former <oncwnuEFk3lVc8alAlO6VwTiNN9I@git.weixin.qq.com>
2020-01-31 12:03:32 +0800

    修复若干bug

--- argue_routine/inc/mobile/activity.inc.php
+++ argue_routine/inc/mobile/activity.inc.php
@@ -9,7 +9,6 @@ switch ($_GPC['action']) {
         $activity_list = pdo_getall('ims_argue_routine_activity');
         /*将参加活动的人数和虚拟人数相加，并转换为以万为单位的数字*/
         foreach ($activity_list as $activity_key => $activity) {
-
             $activity_list[$activity_key] = tidy_activity($activity);
         }
         exit(json_encode($activity_list));
@@ -31,10 +30,12 @@ function tidy_activity($activity)
     /*全部参加人数，虚拟人数加实际人数*/
     $activity['count'] = count(pdo_getall('ims_argue_routine_user', ['activity_id' => $activity['id']])) + $activity['virtual_user'];
     $activity['advertising_img'] = tomedia($activity['advertising_img']);
-    /*是否开始*/
-    $activity['is_start'] = strcmp(date('Y-m-d', time()), $activity['start_time']) === 1;
-    /*是否结束*/
-    $activity['is_end'] = strcmp(date('Y-m-d', time()), $activity['end_time']) === 1;
+    /*分享图片*/
+    $activity['share_img'] = tomedia($activity['share_img']);
+    /*开始时间时间戳*/
+    $activity['start_time_timestamp'] = strtotime($activity['start_time']);
+    /*结束时间时间戳*/
+    $activity['end_time_timestamp'] = strtotime($activity['end_time']);
     /*将奖励规则解码*/
     $activity['bonus_content'] = htmlspecialchars_decode($activity['bonus_content']);
 
