1adb63d7ab6a0290a7a30a98db4738e5e26fa611
Former <oncwnuEFk3lVc8alAlO6VwTiNN9I@git.weixin.qq.com>
2020-03-09 17:25:50 +0800

    图书盘库增加一点功能

--- book_store/inc/mobile/bookrack_manager.inc.php
+++ book_store/inc/mobile/bookrack_manager.inc.php
@@ -3,6 +3,16 @@
 global $_W, $_GPC;
 
 switch ($_GPC['action']) {
+    /*清空指定书架*/
+    case 'empty':
+        $book_list = pdo_getall('ims_book_store_book', ['guild_id' => $_GPC['guild_id'], 'bookrack_id' => $_GPC['bookrack_id']]);
+
+        foreach ($book_list as $book) {
+            pdo_update('ims_book_store_book', ['type' => null], ['id' => $book['id']]);
+        }
+
+        $this->result(0, '清空成功', pdo_getall('ims_book_store_book', ['guild_id' => $_GPC['guild_id'], 'bookrack_id' => $_GPC['bookrack_id']]));
+        break;
     case 'get_all':
         $this->result(0, '获取成功', pdo_getall('ims_book_store_bookrack', ['guild_id' => $_GPC['guild_id']]));
         break;
